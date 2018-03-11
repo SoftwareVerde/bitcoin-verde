4fda892b2d013721d02af53d1d246323f6d4a79e
Josh Green <josh@softwareverde.com>
2018-03-11 18:59:30 -0400

    Updated Main to validate blocks w/ BlockChainSegments.

--- src/main/java/com/softwareverde/bitcoin/server/Main.java
+++ src/main/java/com/softwareverde/bitcoin/server/Main.java
@@ -1,14 +1,12 @@
 package com.softwareverde.bitcoin.server;
 
-import com.softwareverde.bitcoin.block.BlockDeflater;
+import com.softwareverde.bitcoin.block.*;
+import com.softwareverde.bitcoin.chain.segment.BlockChainSegmentId;
 import com.softwareverde.bitcoin.transaction.TransactionInflater;
 import com.softwareverde.bitcoin.transaction.script.stack.ScriptSignature;
 import com.softwareverde.bitcoin.transaction.signer.SignatureContext;
 import com.softwareverde.bitcoin.type.address.Address;
 import com.softwareverde.bitcoin.type.key.PrivateKey;
-import com.softwareverde.bitcoin.block.Block;
-import com.softwareverde.bitcoin.block.BlockInflater;
-import com.softwareverde.bitcoin.block.MutableBlock;
 import com.softwareverde.bitcoin.block.header.difficulty.Difficulty;
 import com.softwareverde.bitcoin.block.header.difficulty.ImmutableDifficulty;
 import com.softwareverde.bitcoin.block.validator.BlockValidator;
@@ -200,88 +198,88 @@ public class Main {
 //            // Public Key: 1HrXm9WZF7LBm3HCwCBgVS3siDbk5DYCuW
 //        }
 
-        // Mine Hardcoded Block...
-        {
-            try {
-                final BlockInflater blockInflater = new BlockInflater();
-
-                final Block previousBlock = blockInflater.fromBytes(BitcoinUtil.hexStringToByteArray("010000006FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D61900000000007DCE47CEB8FC469369F70F2BAEDF22B0377B691FBDF8426E367202FD021A58D2F4569E5AFFFF001D80CFE82A01010000000100000000000000000000000000000000000000000000000000000000000000000000000019184D696E65642076696120426974636F696E2D56657264652EFFFFFFFF0100F2052A010000001E76A619001AF4440149EF4E3936D27C9F54A2AA4EC4F884E14F6B7D5488AC00000000"));
-
-                final TransactionInflater transactionInflater = new TransactionInflater();
-                final Transaction transactionWithinBlock1DoublePrime = transactionInflater.fromBytes(BitcoinUtil.hexStringToByteArray("0100000001736EF73E0AF54C6E0205824DF02F8217930B2E3F436E05192754DEEC0287CE75000000008B483045022100D88F161B7B0774AAB84BA0343B572770FC4E4DC902E9CDDD8D1172C591BDF26F022003AE66DB858F2B5ACE01566E21A1F5A6C0FA4D7AAB7AFBD5BC3057D305931ABA0141044B1F57CD308E8AE8AADA38AA8183A348E1F12C107898760A11324E1B0288C49DE1AB5E1DA16F649B7375046E165FD2CF143F08989F1092A7ED6FED183107B236FFFFFFFF0100F2052A010000001976A914B8E012A1EC221C31F69AA2895129C02C90AAE2C588AC00000000"));
-
-                final PrivateKey privateKey = PrivateKey.parseFromHexString("B6AA8D327D94F746EFB1974E151CA405D4C17EAB4AB4F5CB7757B720D9E62280");
-
-                final MutableBlock prototypeBlock = new MutableBlock();
-                {
-                    final MutableTransactionInput coinbaseTransactionInput = new MutableTransactionInput();
-                    final MutableTransactionOutput coinbaseTransactionOutput = new MutableTransactionOutput();
-                    final MutableTransaction coinbaseTransaction = new MutableTransaction();
-                    {
-                        coinbaseTransactionInput.setPreviousOutputTransactionHash(new MutableHash());
-                        coinbaseTransactionInput.setPreviousOutputIndex(0);
-                        coinbaseTransactionInput.setSequenceNumber(TransactionInput.MAX_SEQUENCE_NUMBER);
-                        coinbaseTransactionInput.setUnlockingScript((new ScriptBuilder()).pushString("Mined via Bitcoin-Verde.").buildUnlockingScript());
-
-                        coinbaseTransactionOutput.setAmount(50L * Transaction.SATOSHIS_PER_BITCOIN);
-                        coinbaseTransactionOutput.setIndex(0);
-                        coinbaseTransactionOutput.setLockingScript((ScriptBuilder.payToAddress(Address.fromPrivateKey(privateKey))));
-
-                        coinbaseTransaction.setVersion(1);
-                        coinbaseTransaction.setLockTime(new ImmutableLockTime(LockTime.MIN_TIMESTAMP));
-                        coinbaseTransaction.setHasWitnessData(false);
-                        coinbaseTransaction.addTransactionInput(coinbaseTransactionInput);
-                        coinbaseTransaction.addTransactionOutput(coinbaseTransactionOutput);
-                    }
-
-                    final MutableTransactionInput newTransactionInput = new MutableTransactionInput();
-                    final MutableTransactionOutput newTransactionOutput = new MutableTransactionOutput();
-                    final MutableTransaction newTransaction = new MutableTransaction();
-                    {
-                        newTransactionInput.setPreviousOutputTransactionHash(transactionWithinBlock1DoublePrime.getHash());
-                        newTransactionInput.setPreviousOutputIndex(0);
-                        newTransactionInput.setSequenceNumber(TransactionInput.MAX_SEQUENCE_NUMBER);
-                        newTransactionInput.setUnlockingScript(Script.EMPTY_SCRIPT);
-
-                        newTransactionOutput.setAmount(50L * Transaction.SATOSHIS_PER_BITCOIN);
-                        newTransactionOutput.setIndex(0);
-                        newTransactionOutput.setLockingScript(ScriptBuilder.payToAddress(Address.fromPrivateKey(PrivateKey.parseFromHexString("D4BF010D3EC25F913CFF91CA34FD4C04A38908E0478B31F669B647EFCD2482A5"))));
-
-                        newTransaction.setVersion(1);
-                        newTransaction.setLockTime(new ImmutableLockTime(LockTime.MIN_TIMESTAMP));
-                        newTransaction.setHasWitnessData(false);
-                        newTransaction.addTransactionInput(newTransactionInput);
-                        newTransaction.addTransactionOutput(newTransactionOutput);
-                    }
-
-                    final TransactionSigner transactionSigner = new TransactionSigner();
-                    final SignatureContext signatureContext = new SignatureContext(newTransaction, ScriptSignature.HashType.SIGNATURE_HASH_ALL); {
-                        signatureContext.setShouldSignInput(0, true, coinbaseTransactionOutput);
-                        signatureContext.setShouldSignOutput(0, true);
-                    }
-                    final Transaction newSignedTransaction = transactionSigner.signTransaction(signatureContext, privateKey);
-
-                    prototypeBlock.setVersion(1);
-                    prototypeBlock.setPreviousBlockHash(previousBlock.getHash());
-                    prototypeBlock.setTimestamp(System.currentTimeMillis() / 1000L);
-                    prototypeBlock.setNonce(0L);
-                    prototypeBlock.setDifficulty(new ImmutableDifficulty(ByteUtil.integerToBytes(Difficulty.BASE_DIFFICULTY_SIGNIFICAND), Difficulty.BASE_DIFFICULTY_EXPONENT));
-                    prototypeBlock.addTransaction(coinbaseTransaction);
-                    prototypeBlock.addTransaction(newSignedTransaction);
-                }
-
-                final Miner miner = new Miner(4, 0);
-                final Block block = miner.mineBlock(prototypeBlock);
-
-                final BlockDeflater blockDeflater = new BlockDeflater();
-                Logger.log(block.getHash());
-                Logger.log(BitcoinUtil.toHexString(blockDeflater.toBytes(block)));
-            }
-            catch (final Exception exception) {
-                exception.printStackTrace();
-            }
-            _exitFailure();
-        }
+//        // Mine Hardcoded Block...
+//        {
+//            try {
+//                final BlockInflater blockInflater = new BlockInflater();
+//
+//                final Block previousBlock = blockInflater.fromBytes(BitcoinUtil.hexStringToByteArray("010000006FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D61900000000007DCE47CEB8FC469369F70F2BAEDF22B0377B691FBDF8426E367202FD021A58D2F4569E5AFFFF001D80CFE82A01010000000100000000000000000000000000000000000000000000000000000000000000000000000019184D696E65642076696120426974636F696E2D56657264652EFFFFFFFF0100F2052A010000001E76A619001AF4440149EF4E3936D27C9F54A2AA4EC4F884E14F6B7D5488AC00000000"));
+//
+//                final TransactionInflater transactionInflater = new TransactionInflater();
+//                final Transaction transactionWithinBlock1DoublePrime = transactionInflater.fromBytes(BitcoinUtil.hexStringToByteArray("0100000001736EF73E0AF54C6E0205824DF02F8217930B2E3F436E05192754DEEC0287CE75000000008B483045022100D88F161B7B0774AAB84BA0343B572770FC4E4DC902E9CDDD8D1172C591BDF26F022003AE66DB858F2B5ACE01566E21A1F5A6C0FA4D7AAB7AFBD5BC3057D305931ABA0141044B1F57CD308E8AE8AADA38AA8183A348E1F12C107898760A11324E1B0288C49DE1AB5E1DA16F649B7375046E165FD2CF143F08989F1092A7ED6FED183107B236FFFFFFFF0100F2052A010000001976A914B8E012A1EC221C31F69AA2895129C02C90AAE2C588AC00000000"));
+//
+//                final PrivateKey privateKey = PrivateKey.parseFromHexString("B6AA8D327D94F746EFB1974E151CA405D4C17EAB4AB4F5CB7757B720D9E62280");
+//
+//                final MutableBlock prototypeBlock = new MutableBlock();
+//                {
+//                    final MutableTransactionInput coinbaseTransactionInput = new MutableTransactionInput();
+//                    final MutableTransactionOutput coinbaseTransactionOutput = new MutableTransactionOutput();
+//                    final MutableTransaction coinbaseTransaction = new MutableTransaction();
+//                    {
+//                        coinbaseTransactionInput.setPreviousOutputTransactionHash(new MutableHash());
+//                        coinbaseTransactionInput.setPreviousOutputIndex(0);
+//                        coinbaseTransactionInput.setSequenceNumber(TransactionInput.MAX_SEQUENCE_NUMBER);
+//                        coinbaseTransactionInput.setUnlockingScript((new ScriptBuilder()).pushString("Mined via Bitcoin-Verde.").buildUnlockingScript());
+//
+//                        coinbaseTransactionOutput.setAmount(50L * Transaction.SATOSHIS_PER_BITCOIN);
+//                        coinbaseTransactionOutput.setIndex(0);
+//                        coinbaseTransactionOutput.setLockingScript((ScriptBuilder.payToAddress(Address.fromPrivateKey(privateKey))));
+//
+//                        coinbaseTransaction.setVersion(1);
+//                        coinbaseTransaction.setLockTime(new ImmutableLockTime(LockTime.MIN_TIMESTAMP));
+//                        coinbaseTransaction.setHasWitnessData(false);
+//                        coinbaseTransaction.addTransactionInput(coinbaseTransactionInput);
+//                        coinbaseTransaction.addTransactionOutput(coinbaseTransactionOutput);
+//                    }
+//
+//                    final MutableTransactionInput newTransactionInput = new MutableTransactionInput();
+//                    final MutableTransactionOutput newTransactionOutput = new MutableTransactionOutput();
+//                    final MutableTransaction newTransaction = new MutableTransaction();
+//                    {
+//                        newTransactionInput.setPreviousOutputTransactionHash(transactionWithinBlock1DoublePrime.getHash());
+//                        newTransactionInput.setPreviousOutputIndex(0);
+//                        newTransactionInput.setSequenceNumber(TransactionInput.MAX_SEQUENCE_NUMBER);
+//                        newTransactionInput.setUnlockingScript(Script.EMPTY_SCRIPT);
+//
+//                        newTransactionOutput.setAmount(50L * Transaction.SATOSHIS_PER_BITCOIN);
+//                        newTransactionOutput.setIndex(0);
+//                        newTransactionOutput.setLockingScript(ScriptBuilder.payToAddress(Address.fromPrivateKey(PrivateKey.parseFromHexString("D4BF010D3EC25F913CFF91CA34FD4C04A38908E0478B31F669B647EFCD2482A5"))));
+//
+//                        newTransaction.setVersion(1);
+//                        newTransaction.setLockTime(new ImmutableLockTime(LockTime.MIN_TIMESTAMP));
+//                        newTransaction.setHasWitnessData(false);
+//                        newTransaction.addTransactionInput(newTransactionInput);
+//                        newTransaction.addTransactionOutput(newTransactionOutput);
+//                    }
+//
+//                    final TransactionSigner transactionSigner = new TransactionSigner();
+//                    final SignatureContext signatureContext = new SignatureContext(newTransaction, ScriptSignature.HashType.SIGNATURE_HASH_ALL); {
+//                        signatureContext.setShouldSignInput(0, true, coinbaseTransactionOutput);
+//                        signatureContext.setShouldSignOutput(0, true);
+//                    }
+//                    final Transaction newSignedTransaction = transactionSigner.signTransaction(signatureContext, privateKey);
+//
+//                    prototypeBlock.setVersion(1);
+//                    prototypeBlock.setPreviousBlockHash(previousBlock.getHash());
+//                    prototypeBlock.setTimestamp(System.currentTimeMillis() / 1000L);
+//                    prototypeBlock.setNonce(0L);
+//                    prototypeBlock.setDifficulty(new ImmutableDifficulty(ByteUtil.integerToBytes(Difficulty.BASE_DIFFICULTY_SIGNIFICAND), Difficulty.BASE_DIFFICULTY_EXPONENT));
+//                    prototypeBlock.addTransaction(coinbaseTransaction);
+//                    prototypeBlock.addTransaction(newSignedTransaction);
+//                }
+//
+//                final Miner miner = new Miner(4, 0);
+//                final Block block = miner.mineBlock(prototypeBlock);
+//
+//                final BlockDeflater blockDeflater = new BlockDeflater();
+//                Logger.log(block.getHash());
+//                Logger.log(BitcoinUtil.toHexString(blockDeflater.toBytes(block)));
+//            }
+//            catch (final Exception exception) {
+//                exception.printStackTrace();
+//            }
+//            _exitFailure();
+//        }
 
         if (commandLineArguments.length != 1) {
             _printUsage();
@@ -343,11 +341,12 @@ public class Main {
                         final BlockChainDatabaseManager blockChainDatabaseManager = new BlockChainDatabaseManager(databaseConnection);
                         final BlockDatabaseManager blockDatabaseManager = new BlockDatabaseManager(databaseConnection);
 
-                        blockDatabaseManager.storeBlock(block);
+                        final BlockId blockId = blockDatabaseManager.storeBlock(block);
                         blockChainDatabaseManager.updateBlockChainsForNewBlock(block);
+                        final BlockChainSegmentId blockChainSegmentId = blockChainDatabaseManager.getBlockChainSegmentId(blockId);
 
                         final BlockValidator blockValidator = new BlockValidator(databaseConnection);
-                        final Boolean blockIsValid = blockValidator.validateBlock(null, block);
+                        final Boolean blockIsValid = blockValidator.validateBlock(blockChainSegmentId, block);
 
                         if (blockIsValid) {
                             TransactionUtil.commitTransaction(databaseConnection);
