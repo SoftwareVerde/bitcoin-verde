a8ca474f94a163b0b1fc3082e8cb17a71b893b36
Josh Green <josh@softwareverde.com>
2018-03-23 13:24:35 -0400

    Fragmenting transactions to facilitate stratum.

--- src/main/java/com/softwareverde/bitcoin/transaction/input/TransactionInputDeflater.java
+++ src/main/java/com/softwareverde/bitcoin/transaction/input/TransactionInputDeflater.java
@@ -1,11 +1,28 @@
 package com.softwareverde.bitcoin.transaction.input;
 
 import com.softwareverde.bitcoin.transaction.script.Script;
+import com.softwareverde.bitcoin.type.bytearray.FragmentedBytes;
 import com.softwareverde.bitcoin.util.ByteUtil;
 import com.softwareverde.bitcoin.util.bytearray.ByteArrayBuilder;
 import com.softwareverde.bitcoin.util.bytearray.Endian;
 
 public class TransactionInputDeflater {
+    private void _toFragmentedBytes(final TransactionInput transactionInput, final ByteArrayBuilder headBytes, final ByteArrayBuilder tailBytes) {
+        final byte[] sequenceBytes = new byte[4];
+        ByteUtil.setBytes(sequenceBytes, ByteUtil.integerToBytes(transactionInput.getSequenceNumber()));
+
+        final byte[] indexBytes = new byte[4];
+        ByteUtil.setBytes(indexBytes, ByteUtil.integerToBytes(transactionInput.getPreviousOutputIndex()));
+
+        final byte[] unlockingScriptBytes = transactionInput.getUnlockingScript().getBytes();
+
+        headBytes.appendBytes(transactionInput.getPreviousOutputTransactionHash().getBytes(), Endian.LITTLE);
+        headBytes.appendBytes(indexBytes, Endian.LITTLE);
+        headBytes.appendBytes(ByteUtil.variableLengthIntegerToBytes(unlockingScriptBytes.length), Endian.BIG);
+        headBytes.appendBytes(unlockingScriptBytes, Endian.BIG);
+
+        tailBytes.appendBytes(sequenceBytes, Endian.LITTLE);
+    }
 
     public Integer getByteCount(final TransactionInput transactionInput) {
         final Integer previousTransactionOutputHashByteCount = 32;
@@ -26,22 +43,17 @@ public class TransactionInputDeflater {
     }
 
     public byte[] toBytes(final TransactionInput transactionInput) {
-        final byte[] sequenceBytes = new byte[4];
-        ByteUtil.setBytes(sequenceBytes, ByteUtil.integerToBytes(transactionInput.getSequenceNumber()));
-
-        final byte[] indexBytes = new byte[4];
-        ByteUtil.setBytes(indexBytes, ByteUtil.integerToBytes(transactionInput.getPreviousOutputIndex()));
-
         final ByteArrayBuilder byteArrayBuilder = new ByteArrayBuilder();
-
-        final byte[] unlockingScriptBytes = transactionInput.getUnlockingScript().getBytes();
-
-        byteArrayBuilder.appendBytes(transactionInput.getPreviousOutputTransactionHash().getBytes(), Endian.LITTLE);
-        byteArrayBuilder.appendBytes(indexBytes, Endian.LITTLE);
-        byteArrayBuilder.appendBytes(ByteUtil.variableLengthIntegerToBytes(unlockingScriptBytes.length), Endian.BIG);
-        byteArrayBuilder.appendBytes(unlockingScriptBytes, Endian.BIG);
-        byteArrayBuilder.appendBytes(sequenceBytes, Endian.LITTLE);
-
+        _toFragmentedBytes(transactionInput, byteArrayBuilder, byteArrayBuilder);
         return byteArrayBuilder.build();
+    }
+
+    public FragmentedBytes fragmentTransactionInput(final TransactionInput transactionInput) {
+        final ByteArrayBuilder headBytesBuilder = new ByteArrayBuilder();
+        final ByteArrayBuilder tailBytesBuilder = new ByteArrayBuilder();
+
+        _toFragmentedBytes(transactionInput, headBytesBuilder, tailBytesBuilder);
+
+        return new FragmentedBytes(headBytesBuilder.build(), tailBytesBuilder.build());
     }
 }
