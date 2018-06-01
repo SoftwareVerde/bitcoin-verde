9ee4fcee604447603e95a28fa3fb9422aa424b8f
Josh Green <josh@softwareverde.com>
2018-06-01 09:35:28 +0200

    Added additional test cases for QueryBlockHeadersHandler.

--- src/test/java/com/softwareverde/bitcoin/server/module/node/QueryBlockHeadersHandlerTests.java
+++ src/test/java/com/softwareverde/bitcoin/server/module/node/QueryBlockHeadersHandlerTests.java
@@ -97,15 +97,30 @@ public class QueryBlockHeadersHandlerTests extends IntegrationTest {
         }
     }
 
+    /**
+     * Creates the following scenario...
+     *
+     *       E      Height: 4
+     *       |
+     *       D      Height: 3
+     *       |
+     *       C      Height: 2
+     *       |
+     *       B      Height: 1
+     *       |
+     *    #1 A      Height: 0
+     *
+     */
     protected Block[] _initScenario() throws Exception {
         final MysqlDatabaseConnection databaseConnection = _database.newConnection();
         final BlockChainDatabaseManager blockChainDatabaseManager = new BlockChainDatabaseManager(databaseConnection);
         final BlockDatabaseManager blockDatabaseManager = new BlockDatabaseManager(databaseConnection);
         final BlockInflater blockInflater = new BlockInflater();
 
-        final Block[] blocks = new Block[6];
+        final String[] blockDatas = new String[]{ BlockData.MainChain.GENESIS_BLOCK, BlockData.MainChain.BLOCK_1, BlockData.MainChain.BLOCK_2, BlockData.MainChain.BLOCK_3, BlockData.MainChain.BLOCK_4 };
+        final Block[] blocks = new Block[blockDatas.length];
         int i = 0;
-        for (final String blockData : new String[]{ BlockData.MainChain.GENESIS_BLOCK, BlockData.MainChain.BLOCK_1, BlockData.MainChain.BLOCK_2, BlockData.MainChain.BLOCK_3, BlockData.MainChain.BLOCK_4, BlockData.MainChain.BLOCK_5 }) {
+        for (final String blockData : blockDatas) {
             final Block block = blockInflater.fromBytes(HexUtil.hexStringToByteArray(blockData));
             blockDatabaseManager.storeBlock(block);
             blockChainDatabaseManager.updateBlockChainsForNewBlock(block);
@@ -116,11 +131,31 @@ public class QueryBlockHeadersHandlerTests extends IntegrationTest {
         return blocks;
     }
 
+    /**
+     * Creates the following scenario...
+     *
+     *     F                        Height: 5
+     *     |
+     *     E         E'             Height: 4
+     *     |         |
+     *  #4 +----D----+ #5           Height: 3
+     *          |
+     *          C         C''       Height: 2
+     *          |         |
+     *       #2 +----B----+ #3      Height: 1
+     *               |
+     *               A #1           Height: 0
+     *
+     */
     protected static Block[] _initScenario2(final Block[] blocks) throws Exception {
         final MysqlDatabaseConnection databaseConnection = _database.newConnection();
         final BlockChainDatabaseManager blockChainDatabaseManager = new BlockChainDatabaseManager(databaseConnection);
         final BlockDatabaseManager blockDatabaseManager = new BlockDatabaseManager(databaseConnection);
         final BlockInflater blockInflater = new BlockInflater();
+
+        final Block block5 = blockInflater.fromBytes(HexUtil.hexStringToByteArray(BlockData.MainChain.BLOCK_5));
+        blockDatabaseManager.storeBlock(block5);
+        blockChainDatabaseManager.updateBlockChainsForNewBlock(block5);
 
         final Block forkedBlock0 = blockInflater.fromBytes(HexUtil.hexStringToByteArray(BlockData.ForkChain3.BLOCK_2));
         blockDatabaseManager.storeBlock(forkedBlock0);
@@ -135,12 +170,26 @@ public class QueryBlockHeadersHandlerTests extends IntegrationTest {
         blockDatabaseManager.storeBlock(forkedBlock1);
         blockChainDatabaseManager.updateBlockChainsForNewBlock(forkedBlock1);
 
-        final Block[] newBlocks = new Block[blocks.length + 2];
+        final Block[] newBlocks = new Block[blocks.length + 3];
         for (int i = 0; i < blocks.length; ++i) {
             newBlocks[i] = blocks[i];
         }
-        newBlocks[blocks.length + 0] = forkedBlock0;
-        newBlocks[blocks.length + 1] = forkedBlock1;
+        newBlocks[blocks.length + 0] = block5;
+        newBlocks[blocks.length + 1] = forkedBlock0;
+        newBlocks[blocks.length + 2] = forkedBlock1;
+
+        { //  Sanity check for the appropriate chain structure...
+            Assert.assertEquals(newBlocks[0].getHash(), newBlocks[1].getPreviousBlockHash());
+            Assert.assertEquals(newBlocks[1].getHash(), newBlocks[2].getPreviousBlockHash());
+            Assert.assertEquals(newBlocks[2].getHash(), newBlocks[3].getPreviousBlockHash());
+            Assert.assertEquals(newBlocks[3].getHash(), newBlocks[4].getPreviousBlockHash());
+            Assert.assertEquals(newBlocks[4].getHash(), newBlocks[5].getPreviousBlockHash());
+
+            Assert.assertEquals(newBlocks[1].getHash(), newBlocks[6].getPreviousBlockHash());
+
+            Assert.assertEquals(newBlocks[3].getHash(), newBlocks[7].getPreviousBlockHash());
+        }
+
         return newBlocks;
     }
 
@@ -152,12 +201,19 @@ public class QueryBlockHeadersHandlerTests extends IntegrationTest {
     @Test
     public void should_return_genesis_blocks_when_no_matches() throws Exception {
         // Setup
-        final Block[] blocks = (_initScenario());
+        final Block[] scenarioBlocks = _initScenario();
+        final Block[] allBlocks = _initScenario2(scenarioBlocks);
+
+        final Integer bestChainHeight = scenarioBlocks.length + 1;
+        final Block[] mainChainBlocks = new Block[bestChainHeight];
+        for (int i = 0; i < scenarioBlocks.length + 1; ++i) { mainChainBlocks[i] = allBlocks[i]; }
 
         final MysqlDatabaseConnectionFactory databaseConnectionFactory = _database.getDatabaseConnectionFactory();
         final QueryBlockHeadersHandler queryBlockHeadersHandler = new QueryBlockHeadersHandler(databaseConnectionFactory);
 
         final FakeNodeConnection fakeNodeConnection = new FakeNodeConnection(new FakeBinarySocket(new FakeSocket()));
+
+        final Integer blockOffset = 0; // The block header/offset that is provided as the last known header...
 
         final List<Sha256Hash> blockHashes = new MutableList<Sha256Hash>();
 
@@ -170,11 +226,11 @@ public class QueryBlockHeadersHandlerTests extends IntegrationTest {
 
         final QueryResponseMessage queryResponseMessage = (QueryResponseMessage) (sentMessages.get(0));
         final List<DataHash> dataHashes = queryResponseMessage.getDataHashes();
-        Assert.assertEquals(blocks.length, dataHashes.getSize());
+        Assert.assertEquals(bestChainHeight - blockOffset, dataHashes.getSize());
 
-        int i = 0;
+        int i = blockOffset;
         for (final DataHash dataHash : dataHashes) {
-            Assert.assertEquals(blocks[i].getHash(), dataHash.getObjectHash());
+            Assert.assertEquals(mainChainBlocks[i].getHash(), dataHash.getObjectHash());
             i += 1;
         }
 
@@ -184,15 +240,22 @@ public class QueryBlockHeadersHandlerTests extends IntegrationTest {
     @Test
     public void should_return_genesis_blocks_when_match_found() throws Exception {
         // Setup
-        final Block[] blocks = (_initScenario());
+        final Block[] scenarioBlocks = _initScenario();
+        final Block[] allBlocks = _initScenario2(scenarioBlocks);
+
+        final Integer bestChainHeight = scenarioBlocks.length + 1;
+        final Block[] mainChainBlocks = new Block[bestChainHeight];
+        for (int i = 0; i < scenarioBlocks.length + 1; ++i) { mainChainBlocks[i] = allBlocks[i]; }
 
         final MysqlDatabaseConnectionFactory databaseConnectionFactory = _database.getDatabaseConnectionFactory();
         final QueryBlockHeadersHandler queryBlockHeadersHandler = new QueryBlockHeadersHandler(databaseConnectionFactory);
 
         final FakeNodeConnection fakeNodeConnection = new FakeNodeConnection(new FakeBinarySocket(new FakeSocket()));
 
+        final Integer blockOffset = 0; // The block header/offset that is provided as the last known header...
+
         final MutableList<Sha256Hash> blockHashes = new MutableList<Sha256Hash>();
-        blockHashes.add(Block.GENESIS_BLOCK_HEADER_HASH);
+        blockHashes.add(allBlocks[blockOffset].getHash());
 
         // Action
         queryBlockHeadersHandler.run(blockHashes, new ImmutableSha256Hash(), fakeNodeConnection);
@@ -203,11 +266,11 @@ public class QueryBlockHeadersHandlerTests extends IntegrationTest {
 
         final QueryResponseMessage queryResponseMessage = (QueryResponseMessage) (sentMessages.get(0));
         final List<DataHash> dataHashes = queryResponseMessage.getDataHashes();
-        Assert.assertEquals(blocks.length, dataHashes.getSize());
+        Assert.assertEquals(bestChainHeight - blockOffset, dataHashes.getSize());
 
-        int i = 0;
+        int i = blockOffset;
         for (final DataHash dataHash : dataHashes) {
-            Assert.assertEquals(blocks[i].getHash(), dataHash.getObjectHash());
+            Assert.assertEquals(mainChainBlocks[i].getHash(), dataHash.getObjectHash());
             i += 1;
         }
 
@@ -217,15 +280,22 @@ public class QueryBlockHeadersHandlerTests extends IntegrationTest {
     @Test
     public void should_return_genesis_blocks_when_non_genesis_match_found() throws Exception {
         // Setup
-        final Block[] blocks = (_initScenario());
+        final Block[] scenarioBlocks = _initScenario();
+        final Block[] allBlocks = _initScenario2(scenarioBlocks);
+
+        final Integer bestChainHeight = scenarioBlocks.length + 1;
+        final Block[] mainChainBlocks = new Block[bestChainHeight];
+        for (int i = 0; i < scenarioBlocks.length + 1; ++i) { mainChainBlocks[i] = allBlocks[i]; }
 
         final MysqlDatabaseConnectionFactory databaseConnectionFactory = _database.getDatabaseConnectionFactory();
         final QueryBlockHeadersHandler queryBlockHeadersHandler = new QueryBlockHeadersHandler(databaseConnectionFactory);
 
         final FakeNodeConnection fakeNodeConnection = new FakeNodeConnection(new FakeBinarySocket(new FakeSocket()));
 
+        final Integer blockOffset = 1; // The block header/offset that is provided as the last known header...
+
         final MutableList<Sha256Hash> blockHashes = new MutableList<Sha256Hash>();
-        blockHashes.add(blocks[1].getHash());
+        blockHashes.add(allBlocks[blockOffset].getHash());
 
         // Action
         queryBlockHeadersHandler.run(blockHashes, new ImmutableSha256Hash(), fakeNodeConnection);
@@ -236,13 +306,102 @@ public class QueryBlockHeadersHandlerTests extends IntegrationTest {
 
         final QueryResponseMessage queryResponseMessage = (QueryResponseMessage) (sentMessages.get(0));
         final List<DataHash> dataHashes = queryResponseMessage.getDataHashes();
-        Assert.assertEquals(blocks.length - 1, dataHashes.getSize());
+        Assert.assertEquals(bestChainHeight - blockOffset, dataHashes.getSize());
 
-        int i = 1;
+        int i = blockOffset;
         for (final DataHash dataHash : dataHashes) {
-            Assert.assertEquals(blocks[i].getHash(), dataHash.getObjectHash());
+            Assert.assertEquals(mainChainBlocks[i].getHash(), dataHash.getObjectHash());
             i += 1;
         }
+
+        fakeNodeConnection.disconnect();
+    }
+
+    @Test
+    public void should_return_blocks_when_match_found() throws Exception {
+        // Setup
+        final Block[] scenarioBlocks = _initScenario();
+        final Block[] allBlocks = _initScenario2(scenarioBlocks);
+
+        final Integer bestChainHeight = scenarioBlocks.length + 1;
+        final Block[] mainChainBlocks = new Block[bestChainHeight];
+        for (int i = 0; i < scenarioBlocks.length + 1; ++i) { mainChainBlocks[i] = allBlocks[i]; }
+
+        final MysqlDatabaseConnectionFactory databaseConnectionFactory = _database.getDatabaseConnectionFactory();
+        final QueryBlockHeadersHandler queryBlockHeadersHandler = new QueryBlockHeadersHandler(databaseConnectionFactory);
+
+        final FakeNodeConnection fakeNodeConnection = new FakeNodeConnection(new FakeBinarySocket(new FakeSocket()));
+
+        final Integer blockOffset = 2; // The block header/offset that is provided as the last known header...
+
+        final MutableList<Sha256Hash> blockHashes = new MutableList<Sha256Hash>();
+        blockHashes.add(allBlocks[blockOffset].getHash());
+
+        // Action
+        queryBlockHeadersHandler.run(blockHashes, new ImmutableSha256Hash(), fakeNodeConnection);
+
+        // Assert
+        final List<ProtocolMessage> sentMessages = fakeNodeConnection.getSentMessages();
+        Assert.assertEquals(1, sentMessages.getSize());
+
+        final QueryResponseMessage queryResponseMessage = (QueryResponseMessage) (sentMessages.get(0));
+        final List<DataHash> dataHashes = queryResponseMessage.getDataHashes();
+        Assert.assertEquals(bestChainHeight - blockOffset, dataHashes.getSize());
+
+        int i = blockOffset;
+        for (final DataHash dataHash : dataHashes) {
+            Assert.assertEquals(mainChainBlocks[i].getHash(), dataHash.getObjectHash());
+            i += 1;
+        }
+
+        fakeNodeConnection.disconnect();
+    }
+
+    @Test
+    public void should_return_forked_blocks_when_match_found() throws Exception {
+        // Setup
+        final Block[] scenarioBlocks = _initScenario();
+        final Block[] allBlocks = _initScenario2(scenarioBlocks);
+
+        final Block extraChildEPrimeBlock;
+        { // Create an additional child onto block E'...
+            final MutableBlock mutableBlock = new MutableBlock(allBlocks[allBlocks.length - 1]);
+            mutableBlock.setPreviousBlockHash(allBlocks[allBlocks.length - 1].getHash());
+
+            final MysqlDatabaseConnection databaseConnection = _database.newConnection();
+            final BlockChainDatabaseManager blockChainDatabaseManager = new BlockChainDatabaseManager(databaseConnection);
+            final BlockDatabaseManager blockDatabaseManager = new BlockDatabaseManager(databaseConnection);
+            blockDatabaseManager.storeBlock(mutableBlock);
+            blockChainDatabaseManager.updateBlockChainsForNewBlock(mutableBlock);
+
+            extraChildEPrimeBlock = mutableBlock;
+        }
+
+        final Integer bestChainHeight = scenarioBlocks.length + 1;
+        final Block[] mainChainBlocks = new Block[bestChainHeight];
+        for (int i = 0; i < scenarioBlocks.length + 1; ++i) { mainChainBlocks[i] = allBlocks[i]; }
+
+        final MysqlDatabaseConnectionFactory databaseConnectionFactory = _database.getDatabaseConnectionFactory();
+        final QueryBlockHeadersHandler queryBlockHeadersHandler = new QueryBlockHeadersHandler(databaseConnectionFactory);
+
+        final FakeNodeConnection fakeNodeConnection = new FakeNodeConnection(new FakeBinarySocket(new FakeSocket()));
+
+        final MutableList<Sha256Hash> blockHashes = new MutableList<Sha256Hash>();
+        blockHashes.add(allBlocks[allBlocks.length - 1].getHash()); // Request the forked block (E')...
+
+        // Action
+        queryBlockHeadersHandler.run(blockHashes, new ImmutableSha256Hash(), fakeNodeConnection);
+
+        // Assert
+        final List<ProtocolMessage> sentMessages = fakeNodeConnection.getSentMessages();
+        Assert.assertEquals(1, sentMessages.getSize());
+
+        final QueryResponseMessage queryResponseMessage = (QueryResponseMessage) (sentMessages.get(0));
+        final List<DataHash> dataHashes = queryResponseMessage.getDataHashes();
+        Assert.assertEquals(2, dataHashes.getSize());
+
+        Assert.assertEquals(allBlocks[allBlocks.length - 1].getHash(), dataHashes.get(dataHashes.getSize() - 2).getObjectHash());
+        Assert.assertEquals(extraChildEPrimeBlock.getHash(), dataHashes.get(dataHashes.getSize() - 1).getObjectHash());
 
         fakeNodeConnection.disconnect();
     }
