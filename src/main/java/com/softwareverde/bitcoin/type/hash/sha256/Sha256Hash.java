8a4d01f9aa75719047c143c6f74e167a1dc400ef
Josh Green <josh@softwareverde.com>
2018-11-14 01:52:55 -0500

    Added SHA256 comparator.

--- src/main/java/com/softwareverde/bitcoin/type/hash/sha256/Sha256Hash.java
+++ src/main/java/com/softwareverde/bitcoin/type/hash/sha256/Sha256Hash.java
@@ -2,9 +2,26 @@ package com.softwareverde.bitcoin.type.hash.sha256;
 
 import com.softwareverde.bitcoin.type.hash.Hash;
 import com.softwareverde.io.Logger;
+import com.softwareverde.util.ByteUtil;
 import com.softwareverde.util.HexUtil;
 
-public interface Sha256Hash extends Hash {
+import java.util.Comparator;
+
+public interface Sha256Hash extends Hash, Comparable<Sha256Hash> {
+    Comparator<Sha256Hash> COMPARATOR = new Comparator<Sha256Hash>() {
+        @Override
+        public int compare(final Sha256Hash hash0, final Sha256Hash hash1) {
+            for (int i = 0; i < BYTE_COUNT; ++i) {
+                final int b0 = ByteUtil.byteToInteger(hash0.getByte(i));
+                final int b1 = ByteUtil.byteToInteger(hash1.getByte(i));
+
+                if (b0 < b1) { return -1; }
+                if (b0 > b1) { return 1; }
+            }
+            return 0;
+        }
+    };
+
     static Sha256Hash fromHexString(final String hexString) {
         if (hexString == null) { return null; }
 
@@ -26,8 +43,13 @@ public interface Sha256Hash extends Hash {
     ImmutableSha256Hash EMPTY_HASH = new ImmutableSha256Hash();
 
     @Override
-    public Sha256Hash toReversedEndian();
+    Sha256Hash toReversedEndian();
 
     @Override
     ImmutableSha256Hash asConst();
+
+    @Override
+    default int compareTo(final Sha256Hash sha256Hash) {
+        return COMPARATOR.compare(this, sha256Hash);
+    }
 }
