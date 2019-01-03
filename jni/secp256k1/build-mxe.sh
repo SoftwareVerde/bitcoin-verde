4dd3f23c84d14266f978d1220bf044cbe8c78160
Josh Green <josh@softwareverde.com>
2019-01-02 22:52:50 -0500

    Added jni binaries for release.

---
+++ jni/secp256k1/build-mxe.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# NOTE:  This script were the commands used to build mxe on debian.
+#   Your mileage may vary.
+
+cd
+
+echo "Building MXE at: `pwd`/mxe"
+
+git clone https://github.com/mxe/mxe.git
+cd mxe
+# https://mxe.cc/#requirements-debian
+sudo apt-get install \
+    autoconf \
+    automake \
+    autopoint \
+    bash \
+    bison \
+    bzip2 \
+    flex \
+    g++ \
+    g++-multilib \
+    gettext \
+    git \
+    gperf \
+    intltool \
+    libc6-dev-i386 \
+    libgdk-pixbuf2.0-dev \
+    libltdl-dev \
+    libssl-dev \
+    libtool-bin \
+    libxml-parser-perl \
+    make \
+    openssl \
+    p7zip-full \
+    patch \
+    perl \
+    pkg-config \
+    python \
+    ruby \
+    sed \
+    unzip \
+    wget \
+    xz-utils
+make cc
+
