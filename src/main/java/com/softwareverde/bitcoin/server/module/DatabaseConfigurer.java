11c0b7132d18cb3af3e851cb42b0a08734816bf9
Josh Green <josh@softwareverde.com>
2019-01-03 08:52:41 -0500

    Updated CSS for iphone.

--- src/main/java/com/softwareverde/bitcoin/server/module/DatabaseConfigurer.java
+++ src/main/java/com/softwareverde/bitcoin/server/module/DatabaseConfigurer.java
@@ -10,12 +10,8 @@ public class DatabaseConfigurer {
         final Integer maxDatabaseThreadCount = Math.max(512, (serverProperties.getMaxPeerCount() * 8));
 
         if (SystemUtil.isWindowsOperatingSystem()) {
+            // MariaDb4j currently only supports 32 bit on Windows, so the log file and memory settings must be less than 2 GB...
             commandLineArguments.setInnoDbBufferPoolByteCount(Math.min(ByteUtil.Unit.GIGABYTES, databaseProperties.getMaxMemoryByteCount()));
-            // commandLineArguments.setInnoDbBufferPoolInstanceCount(4);
-            // commandLineArguments.setInnoDbLogBufferByteCount(ByteUtil.Unit.GIGABYTES);
-            // MariaDb4j currently only supports 32 bit on Windows, so the log file must be less than 2 GB...
-            // commandLineArguments.setInnoDbLogFileByteCount(ByteUtil.Unit.GIGABYTES);
-
             commandLineArguments.setQueryCacheByteCount(0L);
             commandLineArguments.setMaxAllowedPacketByteCount(128 * ByteUtil.Unit.MEGABYTES);
             commandLineArguments.addArgument("--max-connections=" + maxDatabaseThreadCount);
