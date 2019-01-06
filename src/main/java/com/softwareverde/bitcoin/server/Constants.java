2fe4aaf0026df5e1802b1e22c4a47a307d9628cf
Josh Green <josh@softwareverde.com>
2019-01-06 11:29:54 -0500

    Changed user-agent to contain a space instead of a hyphen to aid coin.dance/bitnodes reporting.

--- src/main/java/com/softwareverde/bitcoin/server/Constants.java
+++ src/main/java/com/softwareverde/bitcoin/server/Constants.java
@@ -3,5 +3,5 @@ package com.softwareverde.bitcoin.server;
 public class Constants {
     public static final Integer DATABASE_VERSION = 1;
     public static final Integer PROTOCOL_VERSION = 0x0001117F;
-    public static String USER_AGENT = "/Bitcoin-Verde:1.0.0/";
+    public static String USER_AGENT = "/Bitcoin Verde:1.0.0/";
 }
