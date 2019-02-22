a77f38277eb6c54dbf0b3209845bc66ea147eb4c
Josh Green <josh@softwareverde.com>
2019-02-22 11:41:08 -0500

    Pool Search now (temporarily) forwards to BitcoinVerde.org.

--- stratum/www/js/main.js
+++ stratum/www/js/main.js
@@ -1,5 +1,46 @@
 $(document).ready(function() {
+    Api.search = function(parameters, callback) {
+        const defaultParameters = {
+            query: null
+        };
+        const apiParameters = $.extend({ }, defaultParameters, parameters);
+
+        const query = apiParameters.query;
+
+        const searchInput = $("#search");
+        searchInput.val(query);
+
+        const queryParams = new URLSearchParams(window.location.search);
+        window.location.assign("//bitcoinverde.org/?search=" + window.encodeURIComponent(query));
+    };
+
+    const searchInput = $("#search");
     const loadingImage = $("#search-loading-image");
+
+    searchInput.on("focus", function() {
+        searchInput.select();
+    });
+
+    searchInput.on("keyup", function(event) {
+        const value = searchInput.val();
+        searchInput.css("text-transform", (value.length == 64 ? "uppercase" : "none"));
+    });
+
+    searchInput.on("keypress", function(event) {
+        const value = searchInput.val();
+        if (value.length == 0) { return true; }
+
+        const key = event.which;
+        if (key != KeyCodes.ENTER) { return true; }
+
+        loadingImage.css("visibility", "visible");
+
+        Api.search({ query: value });
+
+        searchInput.blur();
+
+        return false;
+    });
 
     Api.getPrototypeBlock({ }, function(data) {
         loadingImage.css("visibility", "hidden");
