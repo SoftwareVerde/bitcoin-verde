121e24c0d2b8c651e8c26661fd619d03106d84d2
gubatron <gubatron@gmail.com>
2019-05-15 10:50:05 -0600

    Constants USER_AGENT and COINBASE_MESSAGE made final

--- src/main/java/com/softwareverde/bitcoin/server/Constants.java
+++ src/main/java/com/softwareverde/bitcoin/server/Constants.java
@@ -3,6 +3,6 @@ package com.softwareverde.bitcoin.server;
 public class Constants {
     public static final Integer DATABASE_VERSION = 1;
     public static final Integer PROTOCOL_VERSION = 0x0001117F;
-    public static String USER_AGENT = "/Bitcoin Verde:1.0.0/";
-    public static String COINBASE_MESSAGE = "/pool.bitcoinverde.org/";
+    public static final String USER_AGENT = "/Bitcoin Verde:1.0.0/";
+    public static final String COINBASE_MESSAGE = "/pool.bitcoinverde.org/";
 }
