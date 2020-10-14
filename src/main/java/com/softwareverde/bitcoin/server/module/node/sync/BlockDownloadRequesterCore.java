e4b6156393c599b6bd482b0d1d1544d0b4bdd586
Josh Green <josh@softwareverde.com>
2020-10-14 16:06:49 -0400

    Optimized imports.

--- src/main/java/com/softwareverde/bitcoin/server/module/node/sync/BlockDownloadRequesterCore.java
+++ src/main/java/com/softwareverde/bitcoin/server/module/node/sync/BlockDownloadRequesterCore.java
@@ -11,13 +11,11 @@ import com.softwareverde.bitcoin.server.module.node.database.fullnode.FullNodeDa
 import com.softwareverde.bitcoin.server.module.node.manager.BitcoinNodeManager;
 import com.softwareverde.bitcoin.server.module.node.sync.block.BlockDownloader;
 import com.softwareverde.bitcoin.server.module.node.sync.block.pending.PendingBlockId;
-import com.softwareverde.constable.list.List;
 import com.softwareverde.constable.list.mutable.MutableList;
 import com.softwareverde.cryptography.hash.sha256.Sha256Hash;
 import com.softwareverde.database.DatabaseException;
 import com.softwareverde.database.util.TransactionUtil;
 import com.softwareverde.logging.Logger;
-import com.softwareverde.network.p2p.node.NodeId;
 import com.softwareverde.util.type.time.SystemTime;
 
 /**
