60b4981b0fbe9eabcf10232358bd782c9b4f18bc
Josh Green <josh@softwareverde.com>
2021-03-10 14:05:06 -0500

    Updated logging for non-verde statements.

--- src/server/java/com/softwareverde/bitcoin/server/database/DatabaseMaintainer.java
+++ src/server/java/com/softwareverde/bitcoin/server/database/DatabaseMaintainer.java
@@ -5,6 +5,7 @@ import com.softwareverde.constable.list.List;
 import com.softwareverde.constable.list.immutable.ImmutableListBuilder;
 import com.softwareverde.database.DatabaseException;
 import com.softwareverde.logging.Logger;
+import com.softwareverde.util.timer.NanoTimer;
 
 public class DatabaseMaintainer {
     protected final DatabaseConnectionFactory _databaseConnectionFactory;
@@ -12,29 +13,31 @@ public class DatabaseMaintainer {
     protected static final List<String> TABLES;
     static {
         final ImmutableListBuilder<String> listBuilder = new ImmutableListBuilder<String>();
-        listBuilder.add("properties");
-        listBuilder.add("script_types");
-        listBuilder.add("pending_blocks");
-        listBuilder.add("invalid_blocks");
-        listBuilder.add("pending_transactions");
-        listBuilder.add("pending_transaction_data");
-        listBuilder.add("pending_transactions_dependent_transactions");
-        listBuilder.add("addresses");
-        listBuilder.add("blocks");
-        listBuilder.add("blockchain_segments");
-        listBuilder.add("transactions");
         listBuilder.add("block_transactions");
+        listBuilder.add("blockchain_segments");
+        listBuilder.add("blocks");
         listBuilder.add("committed_unspent_transaction_outputs");
-        listBuilder.add("unconfirmed_transactions");
-        listBuilder.add("unconfirmed_transaction_outputs");
-        listBuilder.add("unconfirmed_transaction_inputs");
-        listBuilder.add("indexed_transaction_outputs");
-        listBuilder.add("indexed_transaction_inputs");
-        listBuilder.add("validated_slp_transactions");
+        // listBuilder.add("head_block"); // View, not a Table.
+        // listBuilder.add("head_block_header"); // View, not a Table.
         listBuilder.add("hosts");
-        listBuilder.add("nodes");
+        listBuilder.add("indexed_transaction_inputs");
+        listBuilder.add("indexed_transaction_outputs");
+        listBuilder.add("invalid_blocks");
+        listBuilder.add("metadata");
         listBuilder.add("node_features");
         listBuilder.add("node_transactions_inventory");
+        listBuilder.add("nodes");
+        listBuilder.add("pending_blocks");
+        listBuilder.add("pending_transaction_data");
+        listBuilder.add("pending_transactions");
+        listBuilder.add("pending_transactions_dependent_transactions");
+        listBuilder.add("properties");
+        listBuilder.add("script_types");
+        listBuilder.add("transactions");
+        listBuilder.add("unconfirmed_transaction_inputs");
+        listBuilder.add("unconfirmed_transaction_outputs");
+        listBuilder.add("unconfirmed_transactions");
+        listBuilder.add("validated_slp_transactions");
         TABLES = listBuilder.build();
     }
 
@@ -43,9 +46,14 @@ public class DatabaseMaintainer {
     }
 
     public void analyzeTables() {
+        Logger.info("Analyzing " + TABLES.getCount() + " tables.");
         try (final DatabaseConnection databaseConnection = _databaseConnectionFactory.newConnection()) {
             for (final String tableName : TABLES) {
+                final NanoTimer nanoTimer = new NanoTimer();
+                nanoTimer.start();
                 databaseConnection.executeSql(new Query("ANALYZE TABLE " + tableName));
+                nanoTimer.stop();
+                Logger.trace("Analyzed table " + tableName + " in " + nanoTimer.getMillisecondsElapsed() + "ms.");
             }
         }
         catch (final DatabaseException exception) {
