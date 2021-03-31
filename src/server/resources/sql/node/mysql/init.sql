28d7dcd6f3b796e80bd703d573c8682d518c758a
Josh Green <josh@softwareverde.com>
2021-03-31 09:14:09 -0400

    DSProof table data column is now more appropriately sized.

--- src/server/resources/sql/node/mysql/init.sql
+++ src/server/resources/sql/node/mysql/init.sql
@@ -282,7 +282,7 @@ CREATE TABLE double_spend_proofs (
     hash BINARY(32) NOT NULL,
     transaction_id INT UNSIGNED NOT NULL,
     output_index INT UNSIGNED NOT NULL,
-    data MEDIUMBLOB,
+    data BLOB,
     PRIMARY KEY (hash),
     UNIQUE KEY dsproof_previous_output_ix (transaction_id, output_index)
 ) ENGINE=InnoDB DEFAULT CHARSET=LATIN1;
