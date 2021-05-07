4496ad86fd303fce3504599ffd344589bf27f262
Josh Green <josh@softwareverde.com>
2021-05-06 23:23:32 -0400

    FIX: EciesModule now uses correct input encoding for password.
        This is a backwards-incompatible change.
        EciesModule now has an optional username.

--- src/main/java/com/softwareverde/bitcoin/server/module/EciesModule.java
+++ src/main/java/com/softwareverde/bitcoin/server/module/EciesModule.java
@@ -8,13 +8,15 @@ import com.softwareverde.cryptography.secp256k1.key.PrivateKey;
 import com.softwareverde.cryptography.secp256k1.key.PublicKey;
 import com.softwareverde.cryptography.util.HashUtil;
 import com.softwareverde.logging.Logger;
+import com.softwareverde.util.ByteBuffer;
 import com.softwareverde.util.IoUtil;
+import com.softwareverde.util.StringUtil;
+import com.softwareverde.util.Util;
 
 import java.io.BufferedReader;
 import java.io.Console;
 import java.io.File;
 import java.io.InputStreamReader;
-import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -36,15 +38,27 @@ public class EciesModule implements AutoCloseable {
         }
     }
 
-    protected ByteArray _toBytes(final char[] charArray) {
-        final ByteBuffer byteBuffer = StandardCharsets.UTF_16.encode(CharBuffer.wrap(charArray));
-        final byte[] bytes = byteBuffer.array();
-        return ByteArray.wrap(bytes);
+    protected byte[] _toBytes(final char[] charArray) {
+        final java.nio.ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(CharBuffer.wrap(charArray));
+        return byteBuffer.array();
     }
 
-    protected PrivateKey _derivePrivateKey(final char[] charArray) {
-        final ByteArray passwordBytes = _toBytes(charArray);
-        final Sha256Hash passwordHash = HashUtil.doubleSha256(passwordBytes);
+    protected PrivateKey _derivePrivateKey(final String username, final char[] password) {
+        final ByteBuffer byteBuffer = new ByteBuffer();
+        if (! Util.isBlank(username)) {
+            final byte[] usernameBytes = StringUtil.stringToBytes(username);
+            byteBuffer.appendBytes(usernameBytes, usernameBytes.length);
+
+            final byte[] separatorBytes = StringUtil.stringToBytes(":");
+            byteBuffer.appendBytes(separatorBytes, separatorBytes.length);
+        }
+
+        final byte[] passwordBytes = _toBytes(password);
+        byteBuffer.appendBytes(passwordBytes, passwordBytes.length);
+
+        final int byteCount = byteBuffer.getByteCount();
+        final ByteArray preimage = ByteArray.wrap(byteBuffer.readBytes(byteCount));
+        final Sha256Hash passwordHash = HashUtil.doubleSha256(preimage);
         return PrivateKey.fromBytes(passwordHash);
     }
 
@@ -69,10 +83,14 @@ public class EciesModule implements AutoCloseable {
             return;
         }
 
+        System.out.println("Using a username is optional but increases security.");
+        System.out.print("Username: ");
+        final String username = _readLine();
+
         final Console console = System.console();
-        System.out.print("Enter the password to encrypt: ");
+        System.out.print("Password: ");
         final char[] passwordBytes = console.readPassword();
-        System.out.print("Repeat the password: ");
+        System.out.print("Repeat Password: ");
         final char[] passwordBytes2 = console.readPassword();
         if (! Arrays.equals(passwordBytes, passwordBytes2)) {
             System.err.println("Password mismatch.");
@@ -80,18 +98,23 @@ public class EciesModule implements AutoCloseable {
             return;
         }
 
-        final PrivateKey privateKey = _derivePrivateKey(passwordBytes);
+        final PrivateKey privateKey = _derivePrivateKey(username, passwordBytes);
         final PublicKey publicKey = privateKey.getPublicKey();
 
         final EciesEncrypt eciesEncrypt = new EciesEncrypt(publicKey);
         final ByteArray byteArray = eciesEncrypt.encrypt(fileContents);
+        if (byteArray == null) {
+            System.err.println("Unable to encrypt file.");
+            System.exit(1);
+            return;
+        }
 
         IoUtil.putFileContents(outputFile, byteArray);
         System.out.println("Encrypted contents saved to: " + outputFileName);
     }
 
     protected void _runDecrypt() {
-        System.out.print("Enter the file to decrypt: ");
+        System.out.print("Encrypted Filename: ");
         final String inputFileName = _readLine();
 
         final ByteArray fileContents = ByteArray.wrap(IoUtil.getFileContents(inputFileName));
@@ -101,7 +124,7 @@ public class EciesModule implements AutoCloseable {
             return;
         }
 
-        System.out.print("Enter the destination of the decrypted file: ");
+        System.out.print("Destination: ");
         final String outputFileName = _readLine();
 
         final File outputFile = new File(outputFileName);
@@ -111,14 +134,22 @@ public class EciesModule implements AutoCloseable {
             return;
         }
 
-        System.out.print("Enter the password to decrypt: ");
+        System.out.print("Username: ");
+        final String username = _readLine();
+
+        System.out.print("Password: ");
         final Console console = System.console();
         final char[] passwordBytes = console.readPassword();
 
-        final PrivateKey privateKey = _derivePrivateKey(passwordBytes);
+        final PrivateKey privateKey = _derivePrivateKey(username, passwordBytes);
 
         final EciesDecrypt eciesDecrypt = new EciesDecrypt(privateKey);
         final ByteArray byteArray = eciesDecrypt.decrypt(fileContents);
+        if (byteArray == null) {
+            System.err.println("Unable to decrypt file.");
+            System.exit(1);
+            return;
+        }
 
         IoUtil.putFileContents(outputFile, byteArray);
         System.out.println("Encrypted contents saved to: " + outputFileName);
